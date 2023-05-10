a07b3e7c11db46188daf9d350eb9a2e9bd21aac7
Yonghe Xia <xiayh17@gmail.com>
2023-05-10 13:03:09 +0800

    Update zsh-proxy.plugin.zsh

--- zsh-proxy.plugin.zsh
+++ zsh-proxy.plugin.zsh
@@ -35,16 +35,16 @@ __check_ip() {
 	echo "Check what your IP is"
 	echo "----------------------------------------"
 	echo -n "IPv4: "
-	curl -s -k https://api-ipv4.ip.sb/ip
+	curl -s -k api-ipv4.ip.sb/ip
 	echo "----------------------------------------"
 	echo -n "IPv6: "
-	curl -s -k https://api-ipv6.ip.sb/ip
+	curl -s -k api-ipv6.ip.sb/ip
 
 	if command -v python >/dev/null; then
 		echo ""
 		echo "----------------------------------------"
 		echo "Info: "
-		curl -s -k https://api.ip.sb/geoip | python -m json.tool
+		curl -s -k api.ip.sb/geoip | python -m json.tool
 		echo ""
 	fi
 
